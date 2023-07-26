b454137de1f6a1277e151ff19952aab2b667972c
Miguel-Florian <migflor04@gmail.com>
2023-07-26 17:48:31 +0100

    Update patient create schema

--- src/domain/schemas.py
+++ src/domain/schemas.py
@@ -2,25 +2,6 @@ from datetime import date, datetime
 from typing import List
 from pydantic import BaseModel
 
-from fhir.resources.patient import Patient
-from fhir.resources.humanname import HumanName
-
-from src.domain.entities import Address, Name, Telecom
-from src.domain.valuesobjects import Gender
-
-
-class PatientModel(Patient, BaseModel):
-    # Définissez les champs que vous voulez inclure dans le modèle PatientModel
-    name: List[HumanName]
-
-class HumanNameSchema(BaseModel):
-    text: str
-
-"""class PatientSchema(BaseModel):
-    active: bool
-    name: List[HumanNameSchema]"""
-class PatientModel(Patient):
-    pass
 
 class PatientCreateSchema(BaseModel):
 
@@ -29,12 +10,3 @@ class PatientCreateSchema(BaseModel):
     phone_number: str
     gender: str
     birthdate: date
-
-class PatientSchema(BaseModel):
-    resourceType : str = "Patient"
-    active : bool
-    name: List[Name]
-    telecom: List[Telecom]
-    gender: Gender
-    birthDate: date
-    address: List[Address] 
