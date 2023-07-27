b5b3fce2e07394e221753030891d5597ee340c2e
Miguel-Florian <migflor04@gmail.com>
2023-07-27 05:36:40 +0100

    use eventpublish publish method to publish some events

--- src/services_layers/eventshandler.py
+++ src/services_layers/eventshandler.py
@@ -1,6 +1,7 @@
 from src.domain.events import Event
 from confluent_kafka import Producer
 from src.adapters.kafka.producer import delivery_report
+from src.adapters.eventpublisher import Eventpublisher
 
 #producer = Producer({'bootstrap.servers': 'localhost:9092'})
 class EventHandler(Event):
@@ -9,12 +10,14 @@ class EventHandler(Event):
     def PatientCreated(self,event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush() 
 
     def PatientDeleted(self,event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush() 
     
@@ -22,41 +25,49 @@ class EventHandler(Event):
     def PatientContactAdded(self,event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
     
     def PatientContactRemoved(self, event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
 
     def PatientNameChanged(self, event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
     
     def PatientActivated(self,event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
 
     def PatientDeactivated(self, event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
 
     def PatientGenderChanged(self, event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
     
     def PatientBirthdayChanged(self, event_type, event_data):
         self.event_type = event_type
         self.event_data = event_data
+        Eventpublisher().publish(self.event_type, self.event_data)
+        #Eventpublisher().close()
         #producer.produce(self.event_type, str(self.event_data).encode('utf-8'), callback=delivery_report)
         #producer.flush()
